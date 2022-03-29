9889d84c6871878ba106ebaf0983b753db68d3d3
Leonardo-teodoro <leonardo.teodoro@aluno.cefet-rj.br>
2022-03-29 17:28:24 -0300

    #2 Cria componente da análise

---
+++ src/modules/identification/views/home/components/analysis-area/AnalysisArea.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+      <v-col align="center">
+          <SubTitle>Sobre a Análise</SubTitle>
+      </v-col>
+  </v-container>
+</template>
+
+<script>
+import {SubTitle} from "$shared/components"
+export default {
+    name: "AnalysisArea",   
+    components: {
+        SubTitle
+    },
+
+}
+</script>
