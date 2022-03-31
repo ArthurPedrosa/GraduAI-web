d4cab0f9ff196a60cd293832c43d54893497c2a3
Leonardo-teodoro <leonardo.teodoro@aluno.cefet-rj.br>
2022-03-31 18:18:57 -0300

    Adiciona ícone de informação

--- src/shared/components/icons/index.js
+++ src/shared/components/icons/index.js
@@ -2,5 +2,6 @@ import LogoIcon from "./logo-icon/LogoIcon";
 import WelcomeIcon from "./welcome-icon/WelcomeIcon";
 import GraduateHatIcon from "./graduate-hat-icon/GraduateHatIcon";
 import CreatorsIcon from "./creators-icon/CreatorsIcon";
+import InfoIcon from "./info-icon/InfoIcon";
 
-export { LogoIcon, WelcomeIcon, GraduateHatIcon, CreatorsIcon };
+export { LogoIcon, WelcomeIcon, GraduateHatIcon, CreatorsIcon, InfoIcon };
---
+++ src/shared/components/icons/info-icon/InfoIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg
+    width="34"
+    height="30"
+    viewBox="0 0 34 30"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17 30C26.3888 30 34 23.2843 34 15C34 6.71573 26.3888 0 17 0C7.61116 0 0 6.71573 0 15C0 23.2843 7.61116 30 17 30Z"
+      fill="#48A0DC"
+    />
+    <path
+      d="M17 22.2V23.4"
+      stroke="white"
+      stroke-width="2"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+    />
+    <path
+      d="M12.24 9.59997C12.24 7.26057 14.4078 5.36757 17.068 5.40057C19.5955 5.43177 21.7246 7.30977 21.76 9.54057C21.7838 11.0472 20.9086 12.3744 19.5772 13.1322C17.9214 14.0742 17 15.7566 17 17.4948V19.2"
+      stroke="white"
+      stroke-width="2"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "InfoIcon",
+};
+</script>
