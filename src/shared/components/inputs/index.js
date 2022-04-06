79bd173068cf9c226b2c1b7d56b8bf5407db5f1f
ArthurPedrosa <arthurpedrosaferreira1999@gmail.com>
2022-04-06 19:41:41 -0300

    Importa password #6

--- src/shared/components/inputs/index.js
+++ src/shared/components/inputs/index.js
@@ -1,4 +1,5 @@
 import Input from "./input/Input";
 import InputEmail from "./input-email/InputEmail";
+import InputPassword from "./input-password/InputPassword";
 
-export { Input, InputEmail };
+export { Input, InputEmail, InputPassword };
