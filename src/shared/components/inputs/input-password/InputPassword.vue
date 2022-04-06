7953d544fbf1d8facd5e29f363403ffa82ca32fd
Leonardo-teodoro <71471662+Leonardo-teodoro@users.noreply.github.com>
2022-04-06 19:35:36 -0300

    #6 Cria Input para senhas

---
+++ src/shared/components/inputs/input-password/InputPassword.vue
@@ -0,0 +1,45 @@
+<template>
+  <Input
+    label="Senha"
+    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+    :rules="getRules"
+    @click-append="clickAppend"
+  />
+</template>
+
+<script>
+import { Input } from "$shared/components/inputs";
+export default {
+  name: "InputPassword",
+
+  components: { Input },
+
+  extends: Input,
+
+  computed: {
+    getRules() {
+      return [...this.rules, this.passwordRules];
+    },
+  },
+  props: {
+    type: {
+      type: String,
+      default: "password",
+    },
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    passwordRules: (pValue) =>
+      pValue.length >= 8 || "A senha deve conter 8 ou mais caracteres.",
+    clickAppend: () => {
+      console.log("alo");
+      console.log(this);
+      this.show = !this.show;
+    },
+  },
+};
+</script>
