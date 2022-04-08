8c0172076a60537af7350164c4a69b38a87475a0
Leonardo-teodoro <71471662+Leonardo-teodoro@users.noreply.github.com>
2022-04-08 17:46:24 -0300

    #6 Cria checkbox

---
+++ src/shared/components/inputs/checkbox/Checkbox.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container class="px-0" fluid>
+    <v-row>
+      <v-checkbox v-model="checkboxValue" :label="label"></v-checkbox>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Checkbox",
+
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    value: {
+      default: undefined,
+    },
+  },
+  data() {
+    return {
+      checkboxValue: "",
+    };
+  },
+  mounted() {
+    if (this.value) {
+      this.checkboxValue = this.value;
+    }
+  },
+  watch: {
+    checkboxValue(pValue) {
+      this.$emit("checkbox", pValue);
+    },
+
+    value(pValue) {
+      this.checkboxValue = pValue;
+    },
+  },
+};
+</script>
