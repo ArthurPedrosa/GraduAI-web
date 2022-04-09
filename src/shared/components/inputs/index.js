1f2e7124ad812af996f6f36c728a38c73d35665b
Leonardo-teodoro <71471662+Leonardo-teodoro@users.noreply.github.com>
2022-04-09 16:16:31 -0300

    #6 Cria import para componente de Checkbox

--- src/shared/components/inputs/index.js
+++ src/shared/components/inputs/index.js
@@ -1,5 +1,6 @@
 import Input from "./input/Input";
 import InputEmail from "./input-email/InputEmail";
 import InputPassword from "./input-password/InputPassword";
+import Checkbox from "./checkbox/Checkbox";
 
-export { Input, InputEmail, InputPassword };
+export { Input, InputEmail, InputPassword, Checkbox };
